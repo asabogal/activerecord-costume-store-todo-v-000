17be004ea70a6ce52ae6277dd1925e6146ddbc2d
Alejandro Sabogal <asabogal.git@gmail.com>
2018-06-21 23:32:31 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,8 @@ ActiveRecord::Schema.define(version: 3) do
     t.float "price"
     t.string "size"
     t.string "image_url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
