6e2f456d874be6a45e84d94f616bfee84663e177
Alejandro Sabogal <asabogal.git@gmail.com>
2018-06-21 23:18:03 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,9 @@
-# Create your costume_stores migration here
+class CreateCostumeStores < ActiveRecord::Migration
+
+    def change
+      create_table :costumes do |t|
+        t.string :name
+
+      end
+    end
+end
