5531de22eaa1aac00495305b7df2a662fabe8ea3
Alejandro Sabogal <asabogal.git@gmail.com>
2018-06-21 23:03:21 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,9 +1,9 @@
-# class CreateCostumes < ActiveRecord::Migration
-#
-#     def change
-#       create_table :costumes do |t|
-#         t.string :name
-#
-#       end
-#     end
-# end
+class CreateCostumes < ActiveRecord::Migration
+
+    def change
+      create_table :costumes do |t|
+        t.string :name
+
+      end
+    end
+end
